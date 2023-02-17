2001824715c29b5f9eda09ead951036380d4f0f9
Julien LE BREN <julien@lebren.fr>
2023-02-17 11:26:14 +0100

    Bunch of refactoring

---
+++ lib/settings/presentation/views/settings_delete_page.dart
@@ -0,0 +1,117 @@
+part of settings;
+
+class SettingsDeletePage extends ConsumerWidget {
+  const SettingsDeletePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = ref.watch(flutterAppCoreLocalizationsProvider);
+    final authSettings = ref.watch(authSettingsProvider);
+    final isSaving = ref.watch(
+      settingsDeleteControllerProvider.select((state) => state.isLoading),
+    );
+
+    ref.listen<AuthState>(authStateProvider(authSettings), (_, state) {
+      state.maybeWhen(
+        notAuthed: () {
+          ref.read(currentTabIndexProvider.notifier).state = 0;
+        },
+        orElse: () => null,
+      );
+    });
+
+    return SettingsPageBuilder(
+      title: l10n.settingsDeleteTitle,
+      isSaving: isSaving,
+      child: FormPage(
+        children: [
+          const Icon(
+            Icons.security_update_warning_rounded,
+            size: 100,
+            color: Colors.red,
+          ),
+          Text(
+            l10n.settingsDeleteCaption,
+            style: const TextStyle(color: Colors.grey),
+            textAlign: TextAlign.center,
+          ),
+          const _DeleteButton(),
+          if (kDebugMode) const _LogoutButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class _DeleteButton extends ConsumerWidget {
+  const _DeleteButton({Key? key}) : super(key: key);
+
+  _confirmDeletion(BuildContext context, WidgetRef ref) async {
+    final l10n = ref.watch(flutterAppCoreLocalizationsProvider);
+
+    showPlatformSheetAlert(
+      context: context,
+      ref: ref,
+      title: l10n.settingsDeleteAlertTitle,
+      actions: [
+        AlertAction(
+          title: l10n.settingsDeleteButton,
+          onPressed: () {
+            // Détermine la zone de SignIn dans laquelle on se situe (signIn ou settings),
+            //ce qui influencera certains paramètres comme la couleur des boutons Apple/Google/etc
+            ref.read(signInAreaProvider.notifier).state = SignInArea.signIn;
+
+            final controller =
+                ref.read(settingsDeleteControllerProvider.notifier);
+            controller.delete();
+          },
+          isDestructiveAction: true,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = ref.watch(flutterAppCoreLocalizationsProvider);
+    final isSaving = ref.watch(
+      settingsDeleteControllerProvider.select((state) => state.isLoading),
+    );
+
+    return FormSection(
+      children: [
+        FormCenteredButton(
+          value: l10n.settingsDeleteButton,
+          isDestructive: true,
+          onPressed: isSaving ? null : () => _confirmDeletion(context, ref),
+        ),
+      ],
+    );
+  }
+}
+
+class _LogoutButton extends ConsumerWidget {
+  const _LogoutButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = ref.watch(flutterAppCoreLocalizationsProvider);
+
+    return FormSection(
+      children: [
+        FormCenteredButton(
+          value: l10n.settingsLogoutButton,
+          onPressed: () {
+            // Détermine la zone de SignIn dans laquelle on se situe (signIn ou settings),
+            //ce qui influencera certains paramètres comme la couleur des boutons Apple/Google/etc
+            ref.read(signInAreaProvider.notifier).state = SignInArea.signIn;
+
+            final controller =
+                ref.read(signInButtonsControllerProvider.notifier);
+            controller.signOut();
+          },
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/settings/settings.dart
@@ -0,0 +1,33 @@
+library settings;
+
+import 'dart:async';
+
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_core/extensions/extensions.dart';
+import 'package:flutter_app_core/firebase_auth_service/firebase_auth_service.dart';
+import 'package:flutter_app_core/layout_builder/layout_builder.dart';
+import 'package:flutter_app_core/localization/flutter_app_core_l10n.dart';
+import 'package:flutter_app_core/localization/localization.dart';
+import 'package:flutter_app_core/sign_in/sign_in.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'controllers/settings_account_controller.dart';
+part 'controllers/settings_delete_controller.dart';
+part 'controllers/settings_email_controller.dart';
+part 'controllers/settings_password_controller.dart';
+part 'core/extensions/sign_in_suppliers.dart';
+part 'core/models/settings_state.dart';
+part 'presentation/views/settings_account_page.dart';
+part 'presentation/views/settings_delete_page.dart';
+part 'presentation/views/settings_email_page.dart';
+part 'presentation/views/settings_password_page.dart';
+part 'presentation/widgets/anonymous_badge.dart';
+part 'presentation/widgets/settings_page_builder.dart';
+part 'router/settings_router.dart';
+
+part 'settings.freezed.dart';
