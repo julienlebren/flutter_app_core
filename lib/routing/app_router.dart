24da0859d07a558b48a0cbb497436ebb4b044468
Julien LE BREN <julien@lebren.fr>
2023-02-27 10:53:42 +0100

    Added custom routes to router

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_app_core/firebase_auth_service/firebase_auth_service.dart';
 import 'package:flutter_app_core/layout_builder/layout_builder.dart';
 import 'package:flutter_app_core/sign_in/sign_in.dart';
 import 'package:go_router/go_router.dart';
@@ -23,70 +24,62 @@ List<GoRoute> routes(RoutesRef ref) {
 //@Riverpod(keepAlive: true, dependencies: [routes, authSplash, authSettings])
 //GoRouter goRouter(GoRouterRef ref) {
 final goRouterProvider = Provider<GoRouter>((ref) {
-  final settings = ref.read(authSettingsProvider);
-  final authSplashState = ref.watch(authSplashProvider(settings));
-
   return GoRouter(
     initialLocation: '/',
     debugLogDiagnostics: false,
     //refreshListenable: ref.watch(authStateChangesProvider),
-    routes: authSplashState.maybeWhen(
-      authed: () => ref.watch(routesProvider),
-      orElse: () => [
-        GoRoute(
-          path: '/login',
-          name: SignInRoute.emailLogin.name,
-          pageBuilder: (context, state) => platformPage(
-            key: state.pageKey,
-            child: const SignInEmailLoginPage(),
-          ),
+    routes: [
+      GoRoute(
+        path: '/login',
+        name: SignInRoute.emailLogin.name,
+        pageBuilder: (context, state) => platformPage(
+          key: state.pageKey,
+          child: const SignInEmailLoginPage(),
         ),
-        GoRoute(
-          path: '/register',
-          name: SignInRoute.emailRegister.name,
-          pageBuilder: (context, state) => platformPage(
-            key: state.pageKey,
-            child: const SignInEmailRegisterPage(),
-          ),
+      ),
+      GoRoute(
+        path: '/register',
+        name: SignInRoute.emailRegister.name,
+        pageBuilder: (context, state) => platformPage(
+          key: state.pageKey,
+          child: const SignInEmailRegisterPage(),
         ),
-        GoRoute(
-          path: '/reset',
-          name: SignInRoute.emailReset.name,
-          pageBuilder: (context, state) => platformPage(
-            key: state.pageKey,
-            child: const SignInEmailResetPage(),
-          ),
+      ),
+      GoRoute(
+        path: '/reset',
+        name: SignInRoute.emailReset.name,
+        pageBuilder: (context, state) => platformPage(
+          key: state.pageKey,
+          child: const SignInEmailResetPage(),
         ),
-        GoRoute(
-          path: '/phone',
-          name: SignInRoute.phoneLogin.name,
-          pageBuilder: (context, state) => platformPage(
-            key: state.pageKey,
-            child: const SignInPhonePage(),
-          ),
+      ),
+      GoRoute(
+        path: '/phone',
+        name: SignInRoute.phoneLogin.name,
+        pageBuilder: (context, state) => platformPage(
+          key: state.pageKey,
+          child: const SignInPhonePage(),
         ),
-        GoRoute(
-          path: '/verification',
-          name: SignInRoute.phoneVerification.name,
-          pageBuilder: (context, state) => platformPage(
-            key: state.pageKey,
-            child: const SignInPhoneVerificationPage(),
-          ),
+      ),
+      GoRoute(
+        path: '/verification',
+        name: SignInRoute.phoneVerification.name,
+        pageBuilder: (context, state) => platformPage(
+          key: state.pageKey,
+          child: const SignInPhoneVerificationPage(),
         ),
-        GoRoute(
-          path: '/countries',
-          name: SignInRoute.countries.name,
-          pageBuilder: (context, state) => platformPage(
-            key: state.pageKey,
-            child: const CountriesPage(),
-            fullscreenDialog: true,
-          ),
+      ),
+      GoRoute(
+        path: '/countries',
+        name: SignInRoute.countries.name,
+        pageBuilder: (context, state) => platformPage(
+          key: state.pageKey,
+          child: const CountriesPage(),
+          fullscreenDialog: true,
         ),
-      ],
-    ),
+      ),
+      ...ref.watch(routesProvider),
+    ],
     //errorBuilder: (context, state) => const NotFoundScreen(),
   );
-}, dependencies: [
-  authSettingsProvider,
-  authSplashProvider,
-]);
+});
