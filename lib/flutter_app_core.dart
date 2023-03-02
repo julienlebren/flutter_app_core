708e56c2e27901b4735f6779641678d57ec73e76
Julien LE BREN <julien@lebren.fr>
2023-03-02 11:37:06 +0100

    Added lots of exports

--- lib/flutter_app_core.dart
+++ lib/flutter_app_core.dart
@@ -2,9 +2,6 @@
 
 library flutter_app_core;
 
-//import 'package:flutter/material.dart';
-//import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 export 'ad_manager/ad_manager.dart';
 export 'extensions/extensions.dart';
 export 'firebase_auth_service/firebase_auth_service.dart';
@@ -17,59 +14,24 @@ export 'purchases/purchases.dart';
 export 'settings/settings.dart';
 export 'sign_in/sign_in.dart';
 
-export 'package:go_router/go_router.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:cloud_functions/cloud_functions.dart';
+export 'package:cupertino_icons/cupertino_icons.dart';
+export 'package:dio/dio.dart';
+export 'package:firebase_analytics/firebase_analytics.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_core_web/firebase_core_web.dart';
+export 'package:firebase_crashlytics/firebase_crashlytics.dart';
+export 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+export 'package:firebase_messaging/firebase_messaging.dart';
+export 'package:firebase_storage/firebase_storage.dart';
 export 'package:flutter_riverpod/flutter_riverpod.dart';
-export 'package:riverpod_annotation/riverpod_annotation.dart';
 export 'package:freezed_annotation/freezed_annotation.dart';
-
-/*
-class App extends ConsumerWidget {
-  const App({Key? key}) : super(key: key);
-
-  final AppTheme theme;
-  final Purc
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    ref.listen<User?>(userProvider, (_, user) {
-      if (user != null) {
-        FirebaseCrashlytics.instance.setUserIdentifier(user.id!);
-      }
-    });
-
-    return ProviderScope(
-      overrides: [
-        appThemeProvider.overrideWith(mcjTheme),
-        purchasesSettingsProvider.overrideWith(mcjPurchasesSettings),
-        authSettingsProvider.overrideWith(mcjAuthSettings),
-        fcmTokenHandlerProvider.overrideWith(mcjTokenHandler),
-        messageHandlerProvider.overrideWith(mcjMessageHandler),
-        adSettingsProvider.overrideWith(mcjAdSettings),
-        intersticialAdDisplayProvider.overrideWith(mcjIntersticialAdDisplay),
-        signInThemeProvider.overrideWith(mcjSignInTheme),
-        signInCustomRouterProvider.overrideWithValue(
-          SignInCustomRouter.onGenerateRoute,
-        ),
-      ],
-      child: PlatformApp(
-        navigatorKey: NavigatorKeys.main,
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        onGenerateRoute: (settings) => AppRouter.onGenerateRoute(settings, ref),
-        builder: (context, child) {
-          return ProviderScope(
-            overrides: [
-              localizationProvider
-                  .overrideWithValue(AppLocalizations.of(context)!),
-              networkSettingsProvider.overrideWith(mcjNetworkSettings),
-            ],
-            child: child!,
-          );
-        },
-        initialRoute: AppRoutes.splashPage,
-        //home: const SplashPage(),
-      ),
-    );
-  }
-}
-*/
+export 'package:google_fonts/google_fonts.dart';
+export 'package:go_router/go_router.dart';
+export 'package:intl/intl.dart';
+export 'package:json_annotation/json_annotation.dart';
+export 'package:riverpod_annotation/riverpod_annotation.dart';
+export 'package:stack_trace/stack_trace.dart';
+export 'package:url_launcher/url_launcher.dart';
