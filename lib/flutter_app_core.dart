14175596c5bbf93019d301476c7806f2318e0093
Julien LE BREN <julien@lebren.fr>
2023-03-02 18:28:32 +0100

    Removed firebase_core_web from exports

--- lib/flutter_app_core.dart
+++ lib/flutter_app_core.dart
@@ -20,7 +20,6 @@ export 'package:cupertino_icons/cupertino_icons.dart';
 export 'package:dio/dio.dart';
 export 'package:firebase_analytics/firebase_analytics.dart';
 export 'package:firebase_core/firebase_core.dart';
-export 'package:firebase_core_web/firebase_core_web.dart';
 export 'package:firebase_crashlytics/firebase_crashlytics.dart';
 export 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 export 'package:firebase_messaging/firebase_messaging.dart';
