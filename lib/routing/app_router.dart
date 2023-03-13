c2abecc5d1de022cb036595490ea49ddfd420850
Julien LE BREN <julien@lebren.fr>
2023-03-13 11:15:50 +0100

    Tests on ShellRoute with CupertinoTabScaffold

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -78,7 +78,7 @@ Page modalTransition(
 // ignore: unsupported_provider_value
 GoRouter goRouter(
   GoRouterRef ref, {
-  String initialLocation = '/',
+  String initialLocation = '/a',
 }) {
   final mainRoute = ref.watch(routeProvider);
 
@@ -87,6 +87,70 @@ GoRouter goRouter(
     initialLocation: initialLocation,
     debugLogDiagnostics: false,
     routes: [
+      ShellRoute(
+        navigatorKey: NavigatorKeys.tab,
+        builder: (BuildContext context, GoRouterState state, Widget child) {
+          return ScaffoldWithNavBar(child: child);
+        },
+        routes: [
+          /// The first screen to display in the bottom navigation bar.
+          GoRoute(
+            path: '/a',
+            builder: (BuildContext context, GoRouterState state) {
+              return const ScreenA();
+            },
+            routes: <RouteBase>[
+              // The details screen to display stacked on the inner Navigator.
+              // This will cover screen A but not the application shell.
+              GoRoute(
+                path: 'details',
+                builder: (BuildContext context, GoRouterState state) {
+                  return const DetailsScreen(label: 'A');
+                },
+              ),
+            ],
+          ),
+
+          /// Displayed when the second item in the the bottom navigation bar is
+          /// selected.
+          GoRoute(
+            path: '/b',
+            builder: (BuildContext context, GoRouterState state) {
+              return const ScreenB();
+            },
+            routes: <RouteBase>[
+              /// Same as "/a/details", but displayed on the root Navigator by
+              /// specifying [parentNavigatorKey]. This will cover both screen B
+              /// and the application shell.
+              GoRoute(
+                path: 'details',
+                parentNavigatorKey: NavigatorKeys.root,
+                builder: (BuildContext context, GoRouterState state) {
+                  return const DetailsScreen(label: 'B');
+                },
+              ),
+            ],
+          ),
+
+          /// The third screen to display in the bottom navigation bar.
+          GoRoute(
+            path: '/c',
+            builder: (BuildContext context, GoRouterState state) {
+              return const ScreenC();
+            },
+            routes: <RouteBase>[
+              // The details screen to display stacked on the inner Navigator.
+              // This will cover screen A but not the application shell.
+              GoRoute(
+                path: 'details',
+                builder: (BuildContext context, GoRouterState state) {
+                  return const DetailsScreen(label: 'C');
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
       GoRoute(
         path: '/',
         name: SignInRoute.landing.name,
@@ -95,70 +159,6 @@ GoRouter goRouter(
           return NoTransitionPage(child: mainRoute.builder!(context, state));
         },
         routes: [
-          ShellRoute(
-            navigatorKey: NavigatorKeys.tab,
-            builder: (BuildContext context, GoRouterState state, Widget child) {
-              return ScaffoldWithNavBar(child: child);
-            },
-            routes: [
-              /// The first screen to display in the bottom navigation bar.
-              GoRoute(
-                path: '/a',
-                builder: (BuildContext context, GoRouterState state) {
-                  return const ScreenA();
-                },
-                routes: <RouteBase>[
-                  // The details screen to display stacked on the inner Navigator.
-                  // This will cover screen A but not the application shell.
-                  GoRoute(
-                    path: 'details',
-                    builder: (BuildContext context, GoRouterState state) {
-                      return const DetailsScreen(label: 'A');
-                    },
-                  ),
-                ],
-              ),
-
-              /// Displayed when the second item in the the bottom navigation bar is
-              /// selected.
-              GoRoute(
-                path: '/b',
-                builder: (BuildContext context, GoRouterState state) {
-                  return const ScreenB();
-                },
-                routes: <RouteBase>[
-                  /// Same as "/a/details", but displayed on the root Navigator by
-                  /// specifying [parentNavigatorKey]. This will cover both screen B
-                  /// and the application shell.
-                  GoRoute(
-                    path: 'details',
-                    parentNavigatorKey: NavigatorKeys.root,
-                    builder: (BuildContext context, GoRouterState state) {
-                      return const DetailsScreen(label: 'B');
-                    },
-                  ),
-                ],
-              ),
-
-              /// The third screen to display in the bottom navigation bar.
-              GoRoute(
-                path: '/c',
-                builder: (BuildContext context, GoRouterState state) {
-                  return const ScreenC();
-                },
-                routes: <RouteBase>[
-                  // The details screen to display stacked on the inner Navigator.
-                  // This will cover screen A but not the application shell.
-                  GoRoute(
-                    path: 'details',
-                    builder: (BuildContext context, GoRouterState state) {
-                      return const DetailsScreen(label: 'C');
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
           ShellRoute(
             navigatorKey: NavigatorKeys.signIn,
             pageBuilder: (context, state, child) {
