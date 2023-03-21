257ae2d22199bab84a880140b0e9ec5fc6d4e436
Julien LE BREN <julien@lebren.fr>
2023-03-21 16:07:18 +0100

    Tests on padding on DialogRoute

--- lib/routing/app_router.g.dart
+++ lib/routing/app_router.g.dart
@@ -20,7 +20,7 @@ final routeProvider = Provider<GoRoute>.internal(
 );
 
 typedef RouteRef = ProviderRef<GoRoute>;
-String _$goRouterHash() => r'6b7f3d3bda8f06e783a13b6d616f320173215ee6';
+String _$goRouterHash() => r'98f94f11fdca2da4e50f280a1e943bf9f6b467d9';
 
 /// See also [goRouter].
 @ProviderFor(goRouter)
@@ -34,33 +34,4 @@ final goRouterProvider = Provider<GoRouter>.internal(
 );
 
 typedef GoRouterRef = ProviderRef<GoRouter>;
-String _$signInRouterHash() => r'68be083049ad7fe3d1c47b84cb3376f0d88f70a1';
-
-/// See also [signInRouter].
-@ProviderFor(signInRouter)
-final signInRouterProvider = Provider<List<RouteBase>>.internal(
-  signInRouter,
-  name: r'signInRouterProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$signInRouterHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef SignInRouterRef = ProviderRef<List<RouteBase>>;
-String _$settingsRouterHash() => r'fac72f916e712fbe0449541ccc4b48ecf41c09bc';
-
-/// See also [settingsRouter].
-@ProviderFor(settingsRouter)
-final settingsRouterProvider = Provider<List<RouteBase>>.internal(
-  settingsRouter,
-  name: r'settingsRouterProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$settingsRouterHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef SettingsRouterRef = ProviderRef<List<RouteBase>>;
 // ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
